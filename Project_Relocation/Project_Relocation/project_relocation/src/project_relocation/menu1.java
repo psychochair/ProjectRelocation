26743c907400bc8327d3fd47409f0595adc1190a
Marc-Andre Meza <47328015+marque2@users.noreply.github.com>
2019-02-27 09:37:37 -0500

    Updated

--- Project_Relocation/Project_Relocation/project_relocation/src/project_relocation/menu1.java
+++ Project_Relocation/Project_Relocation/project_relocation/src/project_relocation/menu1.java
@@ -5,10 +5,15 @@
  */
 package project_relocation;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 /**
  *
@@ -21,7 +26,7 @@ public class menu1 extends BorderPane implements UI{
         
         //GRID PANE FOR THE TOP MENU 1
         GridPane gridPaneTop = new GridPane();
-        Label titleName = new Label("Projetc_Relocation");
+        Label titleName = new Label("Project_Relocation");
         gridPaneTop.setAlignment(Pos.CENTER);
         gridPaneTop.add(titleName, 1, 0);
         
@@ -29,18 +34,45 @@ public class menu1 extends BorderPane implements UI{
         //GRID PANE FOR THE CENTER MENU 1
         GridPane gridPaneMenus = new GridPane();
         gridPaneMenus.setAlignment(Pos.CENTER);
-        Label newGameLabel = new Label("New Game");
-        Label loadGameLabel = new Label("Load Game");
-        gridPaneMenus.add(newGameLabel, 0, 0);
-        gridPaneMenus.add(loadGameLabel,0,1);
         
+        //BUTTONS FOR THE ACTIONS
+        Button newGameButton = new Button("New Game");
+        Button loadGameButton = new Button("Load Game");
+        
+        //ADD BUTTONS TO GRIDPANE
+        gridPaneMenus.add(newGameButton, 0, 0);
+        gridPaneMenus.add(loadGameButton,0,1);
         
         //ADD NODE MENU1 GRID PANE
         this.setCenter(gridPaneMenus);
         this.setTop(gridPaneTop);
+        
+        
+        //BUTTON ACTIONS TO CHANGE SCENE
+        newGameButton.setOnAction(new EventHandler <ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event) {
+            Project_Relocation.setScene(Project_Relocation.getNewGameMenu());
+            }
+        });
+        
+        loadGameButton.setOnAction(new EventHandler <ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event) {
+            Project_Relocation.setScene(Project_Relocation.getLoadGameMenu());
+            }
+        });
+        
+        
+        
+        
+        
+        
+        
    
         
     }
     
     
 }
+
