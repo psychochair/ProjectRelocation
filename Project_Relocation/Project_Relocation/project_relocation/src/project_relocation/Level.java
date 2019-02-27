cdbebfe97a1dbc512fed34d87712a523c10bfccc
Marc-Andre Meza <47328015+marque2@users.noreply.github.com>
2019-02-27 09:56:45 -0500

    Update Level.java

--- Project_Relocation/Project_Relocation/project_relocation/src/project_relocation/Level.java
+++ Project_Relocation/Project_Relocation/project_relocation/src/project_relocation/Level.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package project_relocation;
 
 /**
@@ -5,23 +10,29 @@ package project_relocation;
  * @author sandr
  */
 public class Level {
-    private final double stageWidth=Project_Relocation.sceneWidth;
-    private final double stageHeight=Project_Relocation.sceneHeight;
-    
     
     private String backgroundImgLink= ""; 
     private String floorImgLink= "";
     private Platform[] platforms;
-    private int stageNum;
+    private String name;
     private String gravity;
     private DynamicObject[] dynamicObjs;
     private Obstacle[] obstacles;
     
-    public Level(int sN){        
+    public Level(){
+        
+    }
+    
+    public String getBackgroundImg() {
+        return backgroundImgLink;
     }
 
     public void setBackgroundImg(String backgroundImg) {
-        this.backgroundImgLink = backgroundImgLink;
+        this.backgroundImgLink = backgroundImg;
+    }
+
+    public String getFloorImg() {
+        return floorImgLink;
     }
 
     public void setFloorImg(String floorImg) {
@@ -36,16 +47,24 @@ public class Level {
         this.platforms = platforms;
     }
 
-    public int getStageNum() {
-        return stageNum;
+    public String getName() {
+        return name;
     }
-    
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getGravity() {
         return gravity;
     }
 
     public void setGravity(String game) {
         this.gravity = game;
+    }
+
+    public DynamicObject[] getDynamicObjs() {
+        return dynamicObjs;
     }
 
     public void setDynamicObjs(DynamicObject[] dynamicObjs) {
@@ -62,4 +81,3 @@ public class Level {
     
     
 }
-
