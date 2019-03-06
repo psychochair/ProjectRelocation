f3dfecc23d3b2d70963e6b22079deee810aebed2
Marc-Andre Meza <47328015+marque2@users.noreply.github.com>
2019-03-05 19:57:48 -0500

    Made it into a HBox

--- Project_Relocation/Project_Relocation/project_relocation/src/project_relocation/Timeline.java
+++ Project_Relocation/Project_Relocation/project_relocation/src/project_relocation/Timeline.java
@@ -5,15 +5,44 @@
  */
 package project_relocation;
 
+import java.util.ArrayList;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
 /**
  *
  * @author sandr
  */
-public class Timeline {
+
+
+
+//public Rocket() throws FileNotFoundException{
+//Image img = new Image(new FileInputStream("C:\\Users\\Marc\\Desktop\\rocketProjFinal.png"));
+//this.setFill(new ImagePattern(img));
+//this.setRadiusX(100);
+//this.setRadiusY(100);
+//this.setCenterX(100);
+//this.setCenterY(100);
+//this.setStroke(Color.BLACK);
+//this.setStrokeWidth(1);
+//}
+
+
+
+public class Timeline extends HBox{
     
     private ArrayList<Orders> orders;
     
     public Timeline(){
+    this.setWidth(Project_Relocation.sceneWidth);
+    this.setMinHeight(90);
+    this.setMaxHeight(90);
+this.setStyle("-fx-background-color: #fff;-fx-border-width: 2;" +
+                      "-fx-border-radius: 5;" + 
+                      "-fx-border-color: black;");
+    this.setTranslateX(0);
+        
         
     }
     
