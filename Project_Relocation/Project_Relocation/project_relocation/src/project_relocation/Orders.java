f7fab56204f860b52e0f17cf0ea0aa1f05b7f923
Marc-Andre Meza <47328015+marque2@users.noreply.github.com>
2019-03-05 19:55:21 -0500

    Added properties UI to all orders

--- Project_Relocation/Project_Relocation/project_relocation/src/project_relocation/Orders.java
+++ Project_Relocation/Project_Relocation/project_relocation/src/project_relocation/Orders.java
@@ -5,10 +5,22 @@
  */
 package project_relocation;
 
+import javafx.geometry.Insets;
+import javafx.scene.layout.VBox;
+
 /**
  *
  * @author Marc
  */
-public class Orders {
+public class Orders extends VBox{
+    
+    public Orders(){
+        this.setSpacing(5);
+        this.setStyle("-fx-background-color: #fff;-fx-padding:10;-fx-border-width: 2;" +
+                      "-fx-border-radius: 5;" + 
+                      "-fx-border-color: black;");
+    
+    }
+    
     
 }
