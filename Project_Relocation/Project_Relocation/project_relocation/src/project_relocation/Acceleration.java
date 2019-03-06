9b2a81a424b68adb7a98770036d00ed071cf35b9
Marc-Andre Meza <47328015+marque2@users.noreply.github.com>
2019-03-05 19:53:40 -0500

    Added Block Shape

--- Project_Relocation/Project_Relocation/project_relocation/src/project_relocation/Acceleration.java
+++ Project_Relocation/Project_Relocation/project_relocation/src/project_relocation/Acceleration.java
@@ -5,6 +5,9 @@
  */
 package project_relocation;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 /**
  *
  * @author Marc
@@ -14,6 +17,15 @@ public class Acceleration extends Orders{
     private double accelerationValue = 0;
     private double durationAcceleration = 0;
 
+    public Acceleration(){
+    Label l1 = new Label("Acceleration (m/s)");    
+    TextField tf1 = new TextField();
+    Label l2 = new Label("Duration (s)");    
+    TextField tf2 = new TextField();
+    this.getChildren().addAll(l1,tf1,l2,tf2);
+    
+    }
+    
     public double getAcceleration() {
         return accelerationValue;
     }
