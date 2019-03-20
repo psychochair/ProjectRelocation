68aab9b500e95891890d36848b0343a81092a634
psychochair <47328027+psychochair@users.noreply.github.com>
2019-03-20 10:14:21 -0400

    gucci

--- project_relocation/src/project_relocation/User.java
+++ project_relocation/src/project_relocation/User.java
@@ -1,23 +1,50 @@
 package project_relocation;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+
 /**
  *
  * @author chasa
  */
 
-//YOOOO FAUT TROUVER COMMENT EXTEND ROCKET
 public class User {
     
     private String username;
     private String filePath; 
-    private Rocket rocket; 
+    private int levelsCompleted;
+    private int stars;
+    private int fuelCapacity;
+    private int protection;
+    private int engineType;
     
+    private File userFile=new File("resources/usernames/"+username+".txt");    
     
-    
-    public User(String u, String fP, Rocket r){
+    public User(String u) throws FileNotFoundException, IOException{
         username=u;
-        filePath=fP;
-        rocket=r;      
+        filePath="resources/usernames/"+username+".txt";
+        File userFile=new File(filePath);
+        Scanner reader = new Scanner(userFile);
+        int levelsCompleted = reader.nextInt();
+        int stars = reader.nextInt();
+        int fuelCapacity = reader.nextInt();
+        int protection = reader.nextInt();
+        int engineType = reader.nextInt();
+        
+        System.out.println(levelsCompleted+"\n"+stars+"\n"+fuelCapacity+"\n"+protection+"\n"+engineType);
+        
+        
+        
     }
     
     public String getUsername() {
@@ -31,14 +58,65 @@ public class User {
     public void setFilePath(String filePath) {
         this.filePath = filePath;
     }
-
-    public Rocket getRocket() {
-        return rocket;
-    }
-
-    public void setRocket(Rocket rocket) {
-        this.rocket = rocket;
-    }
     
     
+    
+    public void writeFile() throws FileNotFoundException, IOException{
+        
+        FileOutputStream deleter = new FileOutputStream(filePath);
+        deleter.write(("").getBytes());
+        deleter.close();
+        
+        PrintWriter writer = new PrintWriter(filePath);
+        writer.println(levelsCompleted+"\n"+stars+"\n"+fuelCapacity+"\n"+protection+"\n"+engineType);
+        writer.close();
+        
+    }
+    
+    public int getLevelsCompleted() throws IOException {
+        return levelsCompleted;
+    }
+
+    public void setLevelsCompleted(int levelsCompleted) throws FileNotFoundException, IOException {
+        this.levelsCompleted=levelsCompleted;   
+        writeFile();
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(int stars) throws FileNotFoundException, IOException {
+        this.stars = stars;
+        writeFile();
+    }
+
+    public int getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(int fuelCapacity) throws FileNotFoundException, IOException {
+        this.fuelCapacity = fuelCapacity;
+        writeFile();
+    }
+
+    public int getProtection() {
+        return protection;
+    }
+
+    public void setProtection(int protection) throws FileNotFoundException, IOException {
+        this.protection = protection;
+        writeFile();
+    }
+
+    public int getEngineType() {
+        return engineType;
+    }
+
+    public void setEngineType(int engineType) throws FileNotFoundException, IOException {
+        this.engineType = engineType;
+        writeFile();
+    }
+    
+
 }
