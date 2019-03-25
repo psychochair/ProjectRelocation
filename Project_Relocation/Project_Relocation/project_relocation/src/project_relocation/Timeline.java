61ee8238d14a73dbaf713509aa4ebbe54adfb327
Marc-Andre Meza <47328015+marque2@users.noreply.github.com>
2019-03-25 09:00:41 -0400

    Update Timeline.java

--- Project_Relocation/Project_Relocation/project_relocation/src/project_relocation/Timeline.java
+++ Project_Relocation/Project_Relocation/project_relocation/src/project_relocation/Timeline.java
@@ -6,17 +6,18 @@
 package project_relocation;
 
 import java.util.ArrayList;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 
 /**
  *
  * @author sandr
  */
-
-
-
 //public Rocket() throws FileNotFoundException{
 //Image img = new Image(new FileInputStream("C:\\Users\\Marc\\Desktop\\rocketProjFinal.png"));
 //this.setFill(new ImagePattern(img));
@@ -27,43 +28,162 @@ import javafx.scene.shape.Rectangle;
 //this.setStroke(Color.BLACK);
 //this.setStrokeWidth(1);
 //}
-
-
-
-public class Timeline extends HBox{
+public class Timeline extends Pane {
     
-    private ArrayList<Orders> orders;
     
-    public Timeline(){
-    this.setWidth(Project_Relocation.sceneWidth);
-    this.setMinHeight(90);
-    this.setMaxHeight(90);
-this.setStyle("-fx-background-color: #fff;-fx-border-width: 2;" +
-                      "-fx-border-radius: 5;" + 
-                      "-fx-border-color: black;");
-    this.setTranslateX(0);
+    private static ArrayList<Orders> ordersList = new ArrayList<Orders>();
+
+    public Timeline() {
+        
+        Pane playAroundButton = new Pane();
+        playAroundButton.setStyle("-fx-background-color: #fff;"
+                + "-fx-border-radius: 5;");
+        playAroundButton.setMinWidth(100);
+        playAroundButton.setMaxWidth(100);
+        playAroundButton.setMinHeight(100);
+        playAroundButton.setMaxHeight(100);
+        playAroundButton.setLayoutX(Project_Relocation.sceneWidth - 110);
+        playAroundButton.setLayoutY(10);
+        Polygon triangle = new Polygon();
+        triangle.getPoints().setAll(new Double[]{
+        30.0,20.0
+        ,30.0,80.0
+        ,80.0,50.0}
+        );
+        triangle.setFill(Color.WHITE);
+        
+        Circle playButton = new Circle(50,50,50, Color.RED);
+        playAroundButton.getChildren().addAll(playButton, triangle);
+        playAroundButton.setTranslateZ(300);
         
         
+        
+        
+        
+        this.getChildren().add(playAroundButton);
+        
+        //SET WIDTH AND HEIGHT TIMELINE
+        this.setWidth(Project_Relocation.sceneWidth);
+        this.setMinHeight(120);
+        this.setMaxHeight(120);
+        this.setLayoutX(0);
+        this.setLayoutY(Project_Relocation.sceneHeight - 120);
+
+        //STYLE OF TIMELINE
+        this.setStyle("-fx-background-color: #fff;-fx-border-width: 2;"
+                + "-fx-border-radius: 5;"
+                + "-fx-border-color: black;");
+        this.setTranslateX(0);
+        
+        
+
     }
     
+    public void scrollBlocs(double xvalue){
+        if(!ordersList.isEmpty()){
+                if(ordersList.get(0).getTranslateX()+xvalue<=2){
+     for(int i=0;i<ordersList.size();i++){
+    ordersList.get(i).setTranslateX(ordersList.get(i).getTranslateX()+xvalue);
+    
+    
+    }
+        }
+        
+        }
+
+
+    
+    }
+    
+
     public ArrayList<Orders> getOrders() {
-        return orders;
+        return ordersList;
     }
 
-    public void addOrder(Orders order) {
-      
-    } 
-    
-    public void modifyOrder(Orders[] orders) {
-      
-    } 
-    
-    public void swapOrder(Orders[] orders) {
-      
-    } 
-    
-    public void removeOrder(Orders[] orders) {
-      
-    } 
-}
+    public void addOrder(Orders order, double positionX, double positionY) {        
 
+        if (ordersList.isEmpty()) {
+            order.setTranslateX(2);
+            order.setTranslateY(0 -order.getLayoutY()+Project_Relocation.sceneHeight-120+(60 - order.getHeight()/2));
+            ordersList.add(order);
+            
+
+        } else {
+
+            double blockPositionX = order.getTranslateX();
+            boolean isAlreadyAdded = false;
+            for (int i = 0; i < ordersList.size(); i++) {
+                double timelineX = ordersList.get(i).getTranslateX();
+                if (blockPositionX < timelineX && !isAlreadyAdded) {
+                    ordersList.get(i).setTranslateX(timelineX);
+                    order.setTranslateX(timelineX);
+                    ordersList.add(i, order);
+                    ordersList.get(i).setPositionInTimeline(i+1);
+                    order.positionInTimeline = i;
+                    isAlreadyAdded = true;
+                    order.setTranslateY(-order.getLayoutY()+Project_Relocation.sceneHeight-60-(order.getHeight()/2));
+                } else {
+                    if (isAlreadyAdded) {
+                        ordersList.get(i).setTranslateX(timelineX + 120);
+                        ordersList.get(i).setPositionInTimeline(i);
+                        
+                    }
+
+                }
+
+            }
+            if (!isAlreadyAdded) {
+                order.setTranslateY(-order.getLayoutY()+Project_Relocation.sceneHeight-60-(order.getHeight()/2));      
+                order.setTranslateX(ordersList.get(ordersList.size()-1).getTranslateX()+120);
+                ordersList.add(order);
+                order.positionInTimeline = ordersList.size()-1;
+            }
+
+
+        }
+        System.out.println("SIZE AFTER DROPPED = "+ordersList.size());
+    }
+
+    public void modifyOrder(Orders[] orders) {
+
+    }
+
+    public void swapOrder(Orders[] orders) {
+
+    }
+
+    public static void removeOrder(int x) {
+        ordersList.remove(x);
+        if(x!=ordersList.size()){
+        boolean reachedX = false;
+                    for (int i = 0; i < ordersList.size(); i++) {
+                        
+                        
+                        ordersList.get(i).setPositionInTimeline(i);
+                        
+                        if(i==x){
+                        reachedX = true;
+                        }
+                        if(reachedX){
+                        ordersList.get(i).setTranslateX(ordersList.get(i).getTranslateX()-120);
+                        }
+                
+
+                }
+        }
+        
+        
+        System.out.println("Size after removed = "+ordersList.size());
+        
+                    
+    }
+
+    public double getWidthTimeline() {
+
+        return this.getWidth();
+    }
+    
+    
+    
+
+}
