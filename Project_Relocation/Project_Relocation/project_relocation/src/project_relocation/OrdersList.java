643a250574823344fcfb6b25a506d98feea2dff8
Marc-Andre Meza <47328015+marque2@users.noreply.github.com>
2019-03-25 09:00:20 -0400

    Update OrdersList.java

--- Project_Relocation/Project_Relocation/project_relocation/src/project_relocation/OrdersList.java
+++ Project_Relocation/Project_Relocation/project_relocation/src/project_relocation/OrdersList.java
@@ -5,34 +5,62 @@
  */
 package project_relocation;
 
+import java.util.ArrayList;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
 /**
  *
  * @author Marc
  */
-public class OrdersList extends VBox{
-    
+public class OrdersList extends GridPane{
+   
+
     public OrdersList(){
-    this.setSpacing(5);
-    this.setMaxWidth(110);
-    this.setMinWidth(110);
-    this.setMinHeight(Project_Relocation.sceneHeight - 90);
-    this.setMaxHeight(Project_Relocation.sceneHeight - 90);
+    this.setMaxWidth(120);
+    this.setMinWidth(120);
+    this.setMinHeight(Project_Relocation.sceneHeight - 120);
 this.setStyle("-fx-background-color: #fff;-fx-border-width: 2;" +
                       "-fx-border-radius: 5;" + 
                       "-fx-border-color: black;");
 
-this.setTranslateX(0);
+this.setLayoutX(0);
+this.setLayoutY(0);
 
 //CREATE THE 3 BLOCKS
 Acceleration AccBlock = new Acceleration();
 Wait WaitBlock = new Wait();
 Rotation RotBlock = new Rotation();
 
-this.getChildren().addAll(AccBlock, RotBlock, WaitBlock);
+
+this.add(AccBlock,0,0);
+add(WaitBlock,0,1);
+add(RotBlock,0,2);
+AccBlock.toBack();
+
+
+
+
     }
     
     
+    public void newBlock(Orders x, int row){
+    add(x,0,row);
+    }
+    public void deleteOrder(Orders x){
+    this.getChildren().remove(x);
+    }
     
+    
+    public void scrollBlocs(double xvalue){
+        
+    ((GameUI) getParent()).scrollBlocs(xvalue);
+    }
+    
+    public void sendOrderToTimeline(Orders order, double x, double y){
+    ((GameUI) getParent()).sendOrderToTimeline(order, x, y);
+    
+    }
 }
