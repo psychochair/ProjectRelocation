a221ad82a22b3a9a1883f45eae9b16a28cabe65a
Marc-Andre Meza <47328015+marque2@users.noreply.github.com>
2019-03-25 11:27:17 -0400

    Update Level.java

--- project_relocation/src/project_relocation/Level.java
+++ project_relocation/src/project_relocation/Level.java
@@ -5,7 +5,12 @@
  */
 package project_relocation;
 
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
 
 /**
  *
@@ -21,7 +26,44 @@ public class Level extends Pane{
     private DynamicObject[] dynamicObjs;
     private Obstacle[] obstacles;
     
+    
+    double levelHeight = Project_Relocation.sceneHeight - 120;
+    double levelWidth = Project_Relocation.sceneWidth - 120;
     public Level(){
+        
+        this.setLayoutY(0);
+        this.setLayoutX(120);
+        this.setMinHeight(levelHeight);
+        this.setMinWidth(levelWidth);
+        setHeight(levelHeight);
+        this.setStyle("-fx-border-width: 1;" +
+                      "-fx-border-radius: 5;" + 
+                      "-fx-border-color: black;");
+        
+        
+        
+        
+        
+        Rocket rocketShape = new Rocket();
+        rocketShape.setRadiusX(20);
+        rocketShape.setRadiusY(40);
+        rocketShape.setFill(Color.BLACK);
+      Image rocketImage = new Image("rocketProjFinal.png", 80,80,true,true);
+      ImageView rocketImageView = new ImageView(rocketImage);
+      rocketImageView.setLayoutX(-25.5);
+      rocketImageView.setLayoutY(-44);
+        Group rocketGroup = new Group();
+        rocketGroup.getChildren().addAll(rocketShape, rocketImageView);
+        rocketGroup.setLayoutY(levelHeight - 40);
+        rocketGroup.setLayoutX(levelWidth/2);
+        
+        this.getChildren().add(rocketGroup);
+        
+        
+        
+        
+        
+        
         
     }
     
