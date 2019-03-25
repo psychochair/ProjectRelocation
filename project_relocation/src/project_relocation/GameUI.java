5bc10116401cd57a2acad99840072fb4b4f7f98a
Marc-Andre Meza <47328015+marque2@users.noreply.github.com>
2019-03-25 07:44:16 -0400

    Update GameUI.java

--- project_relocation/src/project_relocation/GameUI.java
+++ project_relocation/src/project_relocation/GameUI.java
@@ -5,6 +5,9 @@
  */
 package project_relocation;
 
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.BorderPane;
 
 /**
@@ -12,13 +15,17 @@ import javafx.scene.layout.BorderPane;
  * @author Marc
  */
 public class GameUI extends BorderPane{
-static Timeline timeline = new Timeline();
+Timeline timeline = new Timeline();
 OrdersList ordersList = new OrdersList();
 
     public GameUI(){
-
-
-
+        
+    timeline.setOnScroll(new EventHandler<ScrollEvent>() {
+        @Override
+        public void handle(ScrollEvent event) {
+            scrollBlocs(event.getDeltaX());
+        }
+    });
 
 this.setBottom(timeline);
 this.setLeft(ordersList);
@@ -27,7 +34,12 @@ this.setLeft(ordersList);
         
 }
 
-public static void sendOrderToTimeline(Orders order, double positionx, double positiony){
+public void sendOrderToTimeline(Orders order, double positionx, double positiony){
 timeline.addOrder(order, positionx, positiony);
+}
+
+public void scrollBlocs(double xvalue){
+timeline.scrollBlocs(xvalue);
+
 }
 }
