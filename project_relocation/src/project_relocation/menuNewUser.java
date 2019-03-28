84c1d6273e9f8cca1972af41221e6301791317ae
psychochair <47328027+psychochair@users.noreply.github.com>
2019-03-28 10:40:06 -0400

    added design

--- project_relocation/src/project_relocation/menuNewUser.java
+++ project_relocation/src/project_relocation/menuNewUser.java
@@ -24,6 +24,8 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -34,6 +36,72 @@ import javafx.scene.layout.VBox;
 public class menuNewUser extends BorderPane implements UI {
     
     public menuNewUser(User user){
+//Design Values
+        int insetSize1=10;
+        
+        int buttonFontSize1=32;
+        String buttonFontFamily1="\"AR Delaney\"";
+        
+        int labelFontSize1=44;
+        String labelFontFamily1="\"AR Delaney\"";
+        
+        int comboBoxFontSize1=28;
+        String comboBoxFontFamily1="\"AR Delaney\"";        
+        
+        
+        String buttonStyle1="-fx-background-color: "
+                    + "linear-gradient(#ffd65b, #e68400), "
+                    + "linear-gradient(#ffef84, #f2ba44), "
+                    + "linear-gradient(#ffea6a, #efaa22), "
+                    + "linear-gradient(#ffe657 0%, #f8c202 50%, #eea10b 100%), "
+                    + "linear-gradient(from 0% 0% to 15% 50%, "
+                    + "rgba(255,255,255,0.9), "
+                    + "rgba(255,255,255,0)); "
+                + "-fx-font-family: "+buttonFontFamily1+"; "
+                + "-fx-background-radius: 30; "
+                + "-fx-background-insets: 0,1,2,3,0; "
+                + "-fx-text-fill: #654b00; "
+                + "-fx-font-weight: bold; "
+                + "-fx-font-size: "+buttonFontSize1+"px; "
+                + "-fx-padding: 10 20 10 20;";
+        
+        String labelStyle1="-fx-font-color: "
+                    + "linear-gradient(#ffd65b, #e68400), "
+                    + "linear-gradient(#ffef84, #f2ba44), "
+                    + "linear-gradient(#ffea6a, #efaa22), "
+                    + "linear-gradient(#ffe657 0%, #f8c202 50%, #eea10b 100%), "
+                    + "linear-gradient(from 0% 0% to 15% 50%, "
+                    + "rgba(255,255,255,0.9), "
+                    + "rgba(255,255,255,0)); "
+                + "-fx-font-family: "+labelFontFamily1+"; "
+                + "-fx-text-fill: #654b00; "
+                + "-fx-font-weight: bold; "
+                + "-fx-font-size: "+labelFontSize1+"px; "
+                + "-fx-padding: 10 20 10 20;";
+        
+        String comboBoxStyle1="-fx-background-color: "
+                    + "linear-gradient(#ffd65b, #e68400), "
+                    + "linear-gradient(#ffef84, #f2ba44), "
+                    + "linear-gradient(#ffea6a, #efaa22), "
+                    + "linear-gradient(#ffe657 0%, #f8c202 50%, #eea10b 100%), "
+                    + "linear-gradient(from 0% 0% to 15% 50%, "
+                    + "rgba(255,255,255,0.9), "
+                    + "rgba(255,255,255,0)); "
+                + "-fx-font-family: "+comboBoxFontFamily1+"; "
+                + "-fx-background-radius: 30; "
+                + "-fx-background-insets: 0,1,2,3,0; "
+                + "-fx-text-fill: #654b00; "
+                + "-fx-font-weight: bold; "
+                + "-fx-font-size: "+comboBoxFontSize1+"px; "
+                + "-fx-padding: 10 20 10 20;";
+        
+//Background image
+        Image background = new Image("menu1Background2.png",1300,650,true,true);
+        ImageView backgroundView = new ImageView(background);
+        
+        this.getChildren().add(backgroundView);
+        
+        
         
 //Creating UserNameFile
         File usernameFile = new File("resources/usernames.txt");
@@ -46,10 +114,13 @@ public class menuNewUser extends BorderPane implements UI {
         HBox buttonBox=new HBox(10);
         
         Label message=new Label("Enter your username, then press confirm!");
+        message.setStyle(labelStyle1);
         TextField usernameInput=new TextField();
         
         Button confirm=new Button("Confirm");
+        confirm.setStyle(buttonStyle1);
         Button back=new Button("Back");
+        back.setStyle(buttonStyle1);
         
         Label errorMessage=new Label("");
         
